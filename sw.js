58620dcf135f9ad0bb7b124b4ee7e63e2c4322ac
posicionaando-maker <posiciona.ando@gmail.com>
2026-05-22 23:03:22 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Permite navegación completa sin internet
 // ==================================================
 
-const CACHE_NAME = 'mi-garaje-v1';
+const CACHE_NAME = 'mi-garaje-v2';
 
 // Archivos a precachear durante la instalación
 const ARCHIVOS_PRECACHE = [
