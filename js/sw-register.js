874c172bd288230c5819dd62e667bc56ebbd4204
posicionaando-maker <posiciona.ando@gmail.com>
2026-05-23 21:18:52 -0400

    Update sw-register.js

--- js/sw-register.js
+++ js/sw-register.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
     // ¡¡¡EL CAMBIO MÁGICO ESTÁ AQUÍ!!!
     // Al añadir '?v=2', el navegador cree que es un archivo nuevo y lo descarga otra vez.
     // Cada vez que hagas cambios importantes, cambia este número (v=3, v=4, etc.)
-    navigator.serviceWorker.register('sw.js?v=2')
+    navigator.serviceWorker.register('sw.js?v=3')
       .then(registration => {
         console.log('✅ Service Worker registrado con éxito.');
       })
