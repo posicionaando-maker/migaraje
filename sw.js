8bc90e6d0fce3fc26ce1fea19933f527a78642ec
posicionaando-maker <posiciona.ando@gmail.com>
2026-06-04 17:19:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // CORREGIDO: Rutas relativas con './' para el precaché
 // ==================================================
 
-const CACHE_NAME = 'mi-garaje-v2';
+const CACHE_NAME = 'mi-garaje-v3';
 
 // CORREGIDO: Archivos a precachear con rutas relativas
 const ARCHIVOS_PRECACHE = [
